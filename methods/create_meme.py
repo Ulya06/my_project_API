e153c31a55a50d42a9ce14f3ebc64c0160126b79
Ulya06 <top-32@mail.ru>
2026-01-06 12:28:12 +0100

    updated project

--- methods/create_meme.py
+++ methods/create_meme.py
@@ -9,5 +9,4 @@ class CreateMeme(MemeEndpoint):
     @allure.step("Create new meme")
     def create_new_meme(self, body):
         self.response = requests.post(self.url, json=body, headers=self.headers)
-        self.json = self.response.json()
         return self.response
--- methods/get_meme.py
+++ methods/get_meme.py
@@ -9,5 +9,4 @@ class GetMeme(MemeEndpoint):
     @allure.step("Get meme by ID")
     def get_meme(self, meme_id):
         self.response = requests.get(f"{self.url}/{meme_id}", headers=self.headers)
-        self.json = self.response.json() if self.response.ok else None
         return self.response
